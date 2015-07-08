5a1cb280003b96345aa84be5be03a49a4f9a929f
Jennifer Jaochico <jenjao515@gmail.com>
2015-07-08 16:16:45 -0700

    can draw line

--- public/js/application.js
+++ public/js/application.js
@@ -12,17 +12,30 @@ var createBox = function() {
  // // the fillStyle property can be a CSS color, gradient, or pattern
  // // the default-style is black
  // // Format ==> variable.fillStyle = 'hextag'
-  context.fillStyle = "#FF0000";
+  context.fillStyle = "#B2EBF2";
    // // fillRect - draws a preset rectangle onto the canvas
  // // Format ==> variable.fillRect(x, y, width, height)
  // // the fillRect will be filled with the color defined in fillStyle
-
+// x & y are the coordinates for what part of the box do you
+// want your rectangle positioned
+// width, height are the actual dimensions of the rectangle you're trying to create
 
   context.fillRect(0,0,150,75);
+};
+
+var drawLine = function() {
+  // moveTo defines the starting point of the line
+  context.moveTo(0, 0);
+  // lineTo defines the ending point of the line
+  context.lineTo(80, 40);
+  // stroke actually draws the line with the coordinates specified
+  // by moveTo and lineTo
+  context.stroke();
 };
 // END FUNCTIONS
 
   createBox();
+  drawLine();
 
 });
 
