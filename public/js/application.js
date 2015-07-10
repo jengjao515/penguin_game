5792b8f3aaba31e17f8a01230fd3f2ede5b2ebd4
Jennifer Jaochico <jenjao515@gmail.com>
2015-07-10 16:45:20 -0700

    add penguin movements

--- public/js/application.js
+++ public/js/application.js
@@ -17,10 +17,10 @@ penguinImage.onload = function() {
   penguinReady = true;
 };
 
-penguinImage.src = "../penguin.png";
+penguinImage.src = "penguin.png";
 
 var penguin = {
-  speed: 256, // movement in pixels per second
+  speed: 400, // movement in pixels per second
   x: 0, // canvas functions in coordinates when placing objects, not pixels
   y: 0
 }
@@ -35,6 +35,28 @@ addEventListener("keyup", function(event){
   delete keysDown[event.keyCode];
 }, false);
 
+///////// Checking for what keys player is pressing ////
+var update = function(modifier) {
+  if (38 in keysDown) {
+    penguin.y -= penguin.speed * modifier;
+  }
+  if (40 in keysDown) {
+    penguin.y += penguin.speed * modifier;
+  }
+  if (37 in keysDown) {
+    penguin.x -= penguin.speed * modifier;
+  }
+  if (39 in keysDown) {
+    penguin.x += penguin.speed * modifier;
+  }
+};
+
+/////// HOW TO RESET GAME ///////
+var reset = function() {
+  penguin.x = canvas.width / 2;
+  penguin.y = canvas.width / 2;
+};
+
 //////////// JS drawing images onto the Canvas /////
 var render = function() {
   if (backgroundReady) {
@@ -44,5 +66,26 @@ var render = function() {
     context.drawImage(penguinImage, penguin.x, penguin.y);
   };
 };
+
+// The main game loop
+var main = function () {
+  var now = Date.now();
+  var delta = now - then;
+
+  update(delta / 1000);
+  render();
+
+  then = now;
+
+  // Request to do this again ASAP
+  requestAnimationFrame(main);
+};
+
+var w = window;
+requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+
+var then = Date.now();
+reset();
+main();
 
 });
